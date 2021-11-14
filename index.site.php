6daa08c3d101ea4b7506db176c6d355160bff825
Hristiyan Dodov <h.dodov@gmail.com>
2021-11-14 22:28:56 +0200

    fix: not found errors when accessing via subfolder URL

--- index.site.php
+++ index.site.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * https://github.com/getkirby/kirby/blob/947a3158b534362256ee7d1d2474f5dca364782b/src/Cms/App.php#L1061
+ * Kirby uses `$_SERVER['SCRIPT_NAME']` and expects this main index file to be
+ * named `index.php`, otherwise it throws `NotFoundException`.
+ */
+$_SERVER['SCRIPT_NAME'] = str_replace('index.site.php', 'index.php', $_SERVER['SCRIPT_NAME']);
+
 require_once 'kirby/bootstrap.php';
 
 echo kirby()->render();
